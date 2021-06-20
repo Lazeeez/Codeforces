381db0d837457b614d6583ef9980156de6768ae7
Lajat5 <64376519+Lazeeez@users.noreply.github.com>
2021-06-20 11:39:06 -0700

    in progress

--- 1538B.cpp
+++ 1538B.cpp
@@ -1,26 +1,34 @@
 #include<iostream>
+#include<numeric>
 #define ll long long int
 using namespace std;
 
 void solve(){
     ll friends, sum = 0;
-    int candles[10000];
+    int candles[10000], num = 0, k;
     cin >> friends;
     for(int i = 0; i < friends; i++)
     {
         cin >> candles[i];
-        sum = sum + candles[i];
     }
+    sum = accumulate(candles, candles + friends, sum);
     if (sum%friends==0)
     {
-        
+        num = sum / friends;
+        for (int i = 0; i < friends; i++)
+        {
+            if (candles[i] != num)
+            {
+                k++;
+            }
+        }
+        cout << k << endl;
     }
     else{
         cout << "-1" << endl;
     }
 
 }
-
 int main(){
     int test_cases;
     cin >> test_cases;
