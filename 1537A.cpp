22b7be9aa513af430ced1a4cef00cda46b08f47e
Lajat5 <64376519+Lazeeez@users.noreply.github.com>
2021-06-20 07:37:06 -0700

    solved

--- 1537A.cpp
+++ 1537A.cpp
@@ -1,46 +1,27 @@
-#include <cstdlib>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
 using namespace std;
-
-void solve(){
-    int length;
-    signed int Array[20000], add = 0, mean, res, res2;
-    cin >> length;
-    for (int i = 0; i < length; i++)
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
     {
-        /* code */
-        cin >> Array[i];
-        add = add + Array[i];
-    } //addition
-        if (add < 0)
-    {
-        /* code */
-        mean = add - length;
-        if (mean == 0)
+        int n;
+        cin>>n;
+        int a[n];
+        int sum=0;
+        for (int j=0; j<n;j++)
         {
-            cout << "0";
+            cin>>a[j];
+            sum=sum+a[j];
         }
-        else
+        if (sum>=n)
         {
-            cout << mean;
-        }            
-    }
-    else
-    {
-        mean = add + length;
-        res = mean + add;
-        res2 = -(res);
-        cout << res2;
-    }
-}
-int main(){
-    int iterator;
-    cin >> iterator;
-    while (iterator--)
-    {
-        solve();
+            cout<<sum-n<<endl;
+        }
+        else 
+        {
+            cout<<1<<endl;
+        }
     }
 }
