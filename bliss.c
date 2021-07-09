292bdc8a735b51f75185e7d0a124c16823e4bebe
Lajat5 <64376519+Lazeeez@users.noreply.github.com>
2021-07-09 19:44:29 +0530

    progress 1

--- bliss.c
+++ bliss.c
@@ -1,7 +1,30 @@
-#include<stdio.h>
-int main(){
-    int i = 10;
-    while(i--){
-        printf("dumb\n");
-    }
+#include <stdio.h>
+main()
+{
+  char filename[20], ch;
+  FILE *fp, *f;
+  clrscr();
+  printf("\nEnter the name of the file");
+  scanf("%s", filename);
+  fp = fopen(filename, "r");
+  f = fopen("dummy", "w");
+  if (fp == NULL)
+  {
+    printf("\nError in reading");
+    getch();
+    return;
+  }
+  while ((ch = fgetc(fp)) != EOF)
+    fputc(ch, f);
+  fclose(fp);
+  fclose(f);
+  printf("\nEnter the name of the file");
+  scanf("%s", filename);
+  f = fopen(filename, "r");
+
+  while ((ch = fgetc(f)) != EOF)
+    fputc(ch, stdout);
+  fclose(f);
+
+  getch();
 }
